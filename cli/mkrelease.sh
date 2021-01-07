998ae7c19fcf1e86d72365704f75404ac20dc45b
Duke Leto <duke@leto.net>
2021-01-06 20:39:23 -0500

    update

--- cli/mkrelease.sh
+++ cli/mkrelease.sh
@@ -32,58 +32,58 @@ cargo clean
 cargo build --release 
 
 # macOS
-rm -rf target/macOS-hushpaperwallet-v$APP_VERSION
-mkdir -p target/macOS-hushpaperwallet-v$APP_VERSION
-cp target/release/hushpaperwallet target/macOS-hushpaperwallet-v$APP_VERSION/
+rm -rf target/macOS-silentdragonpaper-v$APP_VERSION
+mkdir -p target/macOS-silentdragonpaper-v$APP_VERSION
+cp target/release/silentdragonpaper target/macOS-silentdragonpaper-v$APP_VERSION/
 
 # For Windows and Linux, build via docker
-docker run --rm -v $(pwd)/..:/opt/hushpaperwallet rustbuild:latest bash -c "cd /opt/hushpaperwallet/cli && cargo build --release && cargo build --release --target x86_64-pc-windows-gnu && cargo build --release --target aarch64-unknown-linux-gnu"
+docker run --rm -v $(pwd)/..:/opt/silentdragonpaper rustbuild:latest bash -c "cd /opt/silentdragonpaper/cli && cargo build --release && cargo build --release --target x86_64-pc-windows-gnu && cargo build --release --target aarch64-unknown-linux-gnu"
 
 # Now sign and zip the binaries
-gpg --batch --output target/macOS-hushpaperwallet-v$APP_VERSION/hushpaperwallet.sig --detach-sig target/macOS-hushpaperwallet-v$APP_VERSION/hushpaperwallet 
+gpg --batch --output target/macOS-silentdragonpaper-v$APP_VERSION/silentdragonpaper.sig --detach-sig target/macOS-silentdragonpaper-v$APP_VERSION/silentdragonpaper 
 cd target
-cd macOS-hushpaperwallet-v$APP_VERSION
-gsha256sum hushpaperwallet > sha256sum.txt
+cd macOS-silentdragonpaper-v$APP_VERSION
+gsha256sum silentdragonpaper > sha256sum.txt
 cd ..
-zip -r macOS-hushpaperwallet-v$APP_VERSION.zip macOS-hushpaperwallet-v$APP_VERSION 
+zip -r macOS-silentdragonpaper-v$APP_VERSION.zip macOS-silentdragonpaper-v$APP_VERSION 
 cd ..
 
 
 #Linux
-rm -rf target/linux-hushpaperwallet-v$APP_VERSION
-mkdir -p target/linux-hushpaperwallet-v$APP_VERSION
-cp target/release/hushpaperwallet target/linux-hushpaperwallet-v$APP_VERSION/
-gpg --batch --output target/linux-hushpaperwallet-v$APP_VERSION/hushpaperwallet.sig --detach-sig target/linux-hushpaperwallet-v$APP_VERSION/hushpaperwallet
+rm -rf target/linux-silentdragonpaper-v$APP_VERSION
+mkdir -p target/linux-silentdragonpaper-v$APP_VERSION
+cp target/release/silentdragonpaper target/linux-silentdragonpaper-v$APP_VERSION/
+gpg --batch --output target/linux-silentdragonpaper-v$APP_VERSION/silentdragonpaper.sig --detach-sig target/linux-silentdragonpaper-v$APP_VERSION/silentdragonpaper
 cd target
-cd linux-hushpaperwallet-v$APP_VERSION
-gsha256sum hushpaperwallet > sha256sum.txt
+cd linux-silentdragonpaper-v$APP_VERSION
+gsha256sum silentdragonpaper > sha256sum.txt
 cd ..
-zip -r linux-hushpaperwallet-v$APP_VERSION.zip linux-hushpaperwallet-v$APP_VERSION 
+zip -r linux-silentdragonpaper-v$APP_VERSION.zip linux-silentdragonpaper-v$APP_VERSION 
 cd ..
 
 
 #Windows
-rm -rf target/Windows-hushpaperwallet-v$APP_VERSION
-mkdir -p target/Windows-hushpaperwallet-v$APP_VERSION
-cp target/x86_64-pc-windows-gnu/release/hushpaperwallet.exe target/Windows-hushpaperwallet-v$APP_VERSION/
-gpg --batch --output target/Windows-hushpaperwallet-v$APP_VERSION/hushpaperwallet.sig --detach-sig target/Windows-hushpaperwallet-v$APP_VERSION/hushpaperwallet.exe
+rm -rf target/Windows-silentdragonpaper-v$APP_VERSION
+mkdir -p target/Windows-silentdragonpaper-v$APP_VERSION
+cp target/x86_64-pc-windows-gnu/release/silentdragonpaper.exe target/Windows-silentdragonpaper-v$APP_VERSION/
+gpg --batch --output target/Windows-silentdragonpaper-v$APP_VERSION/silentdragonpaper.sig --detach-sig target/Windows-silentdragonpaper-v$APP_VERSION/silentdragonpaper.exe
 cd target
-cd Windows-hushpaperwallet-v$APP_VERSION
-gsha256sum hushpaperwallet.exe > sha256sum.txt
+cd Windows-silentdragonpaper-v$APP_VERSION
+gsha256sum silentdragonpaper.exe > sha256sum.txt
 cd ..
-zip -r Windows-hushpaperwallet-v$APP_VERSION.zip Windows-hushpaperwallet-v$APP_VERSION 
+zip -r Windows-silentdragonpaper-v$APP_VERSION.zip Windows-silentdragonpaper-v$APP_VERSION 
 cd ..
 
 
 # aarch64 (armv8)
-rm -rf target/aarch64-hushpaperwallet-v$APP_VERSION
-mkdir -p target/aarch64-hushpaperwallet-v$APP_VERSION
-cp target/aarch64-unknown-linux-gnu/release/hushpaperwallet target/aarch64-hushpaperwallet-v$APP_VERSION/
-gpg --batch --output target/aarch64-hushpaperwallet-v$APP_VERSION/hushpaperwallet.sig --detach-sig target/aarch64-hushpaperwallet-v$APP_VERSION/hushpaperwallet
+rm -rf target/aarch64-silentdragonpaper-v$APP_VERSION
+mkdir -p target/aarch64-silentdragonpaper-v$APP_VERSION
+cp target/aarch64-unknown-linux-gnu/release/silentdragonpaper target/aarch64-silentdragonpaper-v$APP_VERSION/
+gpg --batch --output target/aarch64-silentdragonpaper-v$APP_VERSION/silentdragonpaper.sig --detach-sig target/aarch64-silentdragonpaper-v$APP_VERSION/silentdragonpaper
 cd target
-cd aarch64-hushpaperwallet-v$APP_VERSION
-gsha256sum hushpaperwallet > sha256sum.txt
+cd aarch64-silentdragonpaper-v$APP_VERSION
+gsha256sum silentdragonpaper > sha256sum.txt
 cd ..
-zip -r aarch64-hushpaperwallet-v$APP_VERSION.zip aarch64-hushpaperwallet-v$APP_VERSION 
+zip -r aarch64-silentdragonpaper-v$APP_VERSION.zip aarch64-silentdragonpaper-v$APP_VERSION 
 cd ..
 
