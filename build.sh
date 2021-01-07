6ca5dc40111ab205557b40cccf45b0597e2746f4
Duke Leto <duke@leto.net>
2021-01-06 20:43:30 -0500

    add build script

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Copyright (c) 2016-2020 The Hush developers
+# Distributed under the GPLv3 software license, see the accompanying
+# file COPYING or https://www.gnu.org/licenses/gpl-3.0.en.html
+
+set -eu -o pipefail
+cd cli
+cargo build --release
